b9dbe78b53f2d15cc64e40b77c79dc431cbbe45d
Alexander N Chin <anc200008@utdallas.edu>
2023-10-27 00:03:56 -0500

    I finished

--- main.py
+++ main.py
@@ -19,16 +19,10 @@ def main():
     train = pd.read_csv(train_file, delimiter='\t', header=0)
     test = pd.read_csv(test_file, delimiter='\t', header=0)
     
-    #add dummy value 1 as first column to train and test
-    train.insert(0, 'dummy', 1)
-    test.insert(0, 'dummy', 1)
-    
     input = (train.iloc[0].values)[:-1]
-    target = (train.iloc[0].values)[-1]
     
     net = Network(int(hidden_layers), int(hidden_nodes), float(learning_rate), len(input))
     net.train(train, test, int(iterations))
-    print(net)
     
 
 
@@ -37,9 +31,9 @@ def sig(input):
 
 
 class Node:
-    def __init__(self, input_size, learningRate):
-        self.weights = [1] + ([0] * input_size)
-        self.value = 0
+    def __init__(self, input_size, learningRate, value):
+        self.weights = ([0] * input_size)
+        self.value = value
         self.delta = 0
         self.learning_rate = learningRate
         self.target = 0
@@ -61,13 +55,13 @@ class Node:
             sum += node.value * self.weights[i]
         self.value = sig(sum)
 
-    def backward(self, next_layer=None):
+    def backward(self, curr_node_index, next_layer=None):
         # calculate node's delta based on last layer's deltas
-        if (next_layer is None):
+        if next_layer is None:
             # if current layer is output layer get real error
             self.delta = self.value * (1 - self.value) * (self.target - self.value)
         else:
-            self.delta = self.value * (1 - self.value) * sum([w * node.delta for w, node in zip(self.weights, next_layer.nodes)])
+            self.delta = self.value * (1 - self.value) * sum([node.weights[curr_node_index] * node.delta for node in next_layer.nodes])
 
     def update_weights(self, prev_layer):
         # update weights based on new delta
@@ -76,9 +70,12 @@ class Node:
 
 
 class Layer:
-    def __init__(self, num_nodes, input_size, learningRate):
+    def __init__(self, num_nodes, input_size, learningRate, add_bias):
         self.size = num_nodes
-        self.nodes = [Node(input_size, learningRate) for x in range(num_nodes)]
+        if add_bias:
+            self.nodes = [Node(input_size + 1, learningRate, 1)] + [Node(input_size + 1, learningRate, 0) for _ in range(num_nodes)]
+        else:
+            self.nodes = [Node(input_size + 1, learningRate, 0) for _ in range(num_nodes)]
         self.learning_rate = learningRate
     def __str__(self):
         return f"{self.nodes}"
@@ -86,20 +83,28 @@ class Layer:
     # set all values in a layer
     def set_values(self, values):
         for i, _ in enumerate(self.nodes):
-            self.nodes[i].set_value(values[i])
+            if i == 0:
+                continue
+            self.nodes[i].set_value(values[i-1])
 
     # set output node's target
     def set_target(self, target):
         self.nodes[0].set_target(target)
 
     # forward propagate layer
-    def forward_prop(self, prev_layer):
-        for node in self.nodes:
+    def forward_prop(self, prev_layer, next_layer):
+        for i, node in enumerate(self.nodes):
+            if i == 0 and next_layer is not None:
+                continue
             node.forward(prev_layer)
 
-    def backward_prop(self, next_layer, prev_layer):
+    def backward_prop(self, next_layer):
+        for i, node in enumerate(self.nodes):
+            # print(self)
+            node.backward(i, next_layer)
+
+    def update_w(self, prev_layer):
         for node in self.nodes:
-            node.backward(next_layer)
             node.update_weights(prev_layer)
 
 
@@ -120,13 +125,13 @@ class Network:
         return message
 
     def add_input_layer(self):
-        self.layers.append(Layer(self.input_size, 0,0))
+        self.layers.append(Layer(self.input_size, 0, 0, True))
 
     def add_hidden_layer(self):
-        self.layers.append(Layer(self.num_hidden_nodes, self.layers[-1].size, self.learn_rate))
+        self.layers.append(Layer(self.num_hidden_nodes, self.layers[-1].size, self.learn_rate, True))
 
     def add_output_layer(self):
-        self.layers.append(Layer(1, self.layers[-1].size,self.learn_rate))
+        self.layers.append(Layer(1, self.layers[-1].size, self.learn_rate, False))
         # add a none at the end to denote output layer
         self.layers.append(None)
 
@@ -141,34 +146,46 @@ class Network:
         self.layers[0].set_values(input)
         # forward propagate
         for i in range(1, len(self.layers) - 1):
-            self.layers[i].forward_prop(self.layers[i - 1])
+            self.layers[i].forward_prop(self.layers[i - 1], self.layers[i + 1])
         # return output of last layer
         return self.layers[-2].nodes[0].value
 
-    def train_instance(self, input, target):
+    def train_instance(self, input, target, iteration, train_df, test_df):
         # set input and classification
         self.layers[0].set_values(input)
         self.layers[-2].set_target(target)
         # forward propagate
         for i in range(1, len(self.layers) - 1):
-            self.layers[i].forward_prop(self.layers[i - 1])
+            self.layers[i].forward_prop(self.layers[i - 1], self.layers[i + 1])
+
+        print(f"At iteration {iteration}")
+        print(f"Forward pass output: {round(self.layers[-2].nodes[0].value, 4):.4f}")
+
         # back propagate
         for i in range(len(self.layers) - 2, 0, -1):
-            self.layers[i].backward_prop(self.layers[i + 1], self.layers[i - 1])
+            self.layers[i].backward_prop(self.layers[i + 1])
+        # update weights
+        for i in range(len(self.layers) - 2, 0, -1):
+            self.layers[i].update_w(self.layers[i - 1])
+
+        # print(self)
+        print(f"Average squared error on training set ({len(train_df)} instances): {round(avg_squared_error(train_df, self), 4):.4f}")
+        print(f"Average squared error on test set ({len(test_df)} instances): {round(avg_squared_error(test_df, self), 4):.4f}\n")
 
     def train(self, input_df, test_df, iterations):
-        for i in range(iterations):
-            
+        iteration = 1
+        while(True):
             for j in range(len(input_df)):
-              input = (input_df.iloc[j].values[:-1]).tolist()
-              target = (input_df.iloc[j].values[-1])
-              self.train_instance(input,target)
-              
-            print(f"At iteration {i}")
-            print(f"Forward pass output: {self.layers[-2].nodes[0].value}")
-            print(f"Average squared error on training set ({len(input_df)} instances): {avg_squared_error(input_df, self)}")
-            print(f"Average squared error on test set ({len(test_df)} instances): {avg_squared_error(test_df, self)}\n")
-            print(self)
+                if iteration > iterations:
+                    return
+                input = (input_df.iloc[j].values[:-1]).tolist()
+                target = (input_df.iloc[j].values[-1])
+                self.train_instance(input, target, iteration, input_df, test_df)
+                iteration += 1
+            # print(f"At iteration {i}")
+            # print(f"Forward pass output: {self.layers[-2].nodes[0].value}")
+            # print(f"Average squared error on training set ({len(input_df)} instances): {avg_squared_error(input_df, self)}")
+            # print(f"Average squared error on test set ({len(test_df)} instances): {avg_squared_error(test_df, self)}\n")
             
 
 def avg_squared_error(df, net):
